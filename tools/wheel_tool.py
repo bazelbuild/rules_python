95cf4fecca3695565d8388174c325c4bd4ff0877
Dillon Giacoppo <dillon@canva.com>
2019-11-04 14:00:00 +1100

    remove top level dirs from import

--- tools/wheel_tool.py
+++ tools/wheel_tool.py
@@ -67,9 +67,6 @@ py_library(
     name = "pkg",
     srcs = glob(["**/*.py"]),
     data = glob(["**/*"], exclude=["**/*.py", "**/* *", "BUILD", "WORKSPACE"]),
-    # This makes this directory a top-level in the python import
-    # search path for anything that depends on this.
-    imports = ["."],
     deps = [{dependencies}],
 )
 
