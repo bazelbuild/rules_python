3f47c03afb53c70c51f305d0ea6ac2c40775c9c2
Gergely Fábián <gergo.fb@gmail.com>
2020-05-20 09:27:15 +0200

    Remove files from py_library data glob() if there is space in the middle of path
    Directory names may also contain spaces in some cases, that would fail
    Bazel.
    Files with spaces in their names were already excluded.

--- packaging/whl.py
+++ packaging/whl.py
@@ -149,7 +149,7 @@ load("{requirements}", "requirement")
 py_library(
     name = "pkg",
     srcs = glob(["**/*.py"]),
-    data = glob(["**/*"], exclude=["**/*.py", "**/* *", "BUILD", "WORKSPACE"]),
+    data = glob(["**/*"], exclude=["**/*.py", "**/* */**", "**/* *", "BUILD", "WORKSPACE"]),
     # This makes this directory a top-level in the python import
     # search path for anything that depends on this.
     imports = ["."],
