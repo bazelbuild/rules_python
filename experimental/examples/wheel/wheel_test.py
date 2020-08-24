5d5a06d738b3dfcb72c1076e1b3955c48fc80d62
Dan Halperin <dan@intentionet.com>
2020-08-23 22:01:34 -0700

    wheel_test.py: fix deprecation warning

--- experimental/examples/wheel/wheel_test.py
+++ experimental/examples/wheel/wheel_test.py
@@ -24,7 +24,7 @@ class WheelTest(unittest.TestCase):
                                 'examples', 'wheel',
                                 'example_minimal_library-0.0.1-py3-none-any.whl')
         with zipfile.ZipFile(filename) as zf:
-            self.assertEquals(
+            self.assertEqual(
                 zf.namelist(),
                 ['experimental/examples/wheel/lib/module_with_data.py',
                  'experimental/examples/wheel/lib/simple_module.py',
@@ -38,7 +38,7 @@ class WheelTest(unittest.TestCase):
                                 'examples', 'wheel',
                                 'example_minimal_package-0.0.1-py3-none-any.whl')
         with zipfile.ZipFile(filename) as zf:
-            self.assertEquals(
+            self.assertEqual(
                 zf.namelist(),
                 ['experimental/examples/wheel/lib/data.txt',
                  'experimental/examples/wheel/lib/module_with_data.py',
@@ -54,7 +54,7 @@ class WheelTest(unittest.TestCase):
                                 'examples', 'wheel',
                                 'example_customized-0.0.1-py3-none-any.whl')
         with zipfile.ZipFile(filename) as zf:
-            self.assertEquals(
+            self.assertEqual(
                 zf.namelist(),
                 ['experimental/examples/wheel/lib/data.txt',
                  'experimental/examples/wheel/lib/module_with_data.py',
@@ -71,7 +71,7 @@ class WheelTest(unittest.TestCase):
             metadata_contents = zf.read(
                 'example_customized-0.0.1.dist-info/METADATA')
             # The entries are guaranteed to be sorted.
-            self.assertEquals(record_contents, b"""\
+            self.assertEqual(record_contents, b"""\
 example_customized-0.0.1.dist-info/METADATA,sha256=TeeEmokHE2NWjkaMcVJuSAq4_AXUoIad2-SLuquRmbg,372
 example_customized-0.0.1.dist-info/RECORD,,
 example_customized-0.0.1.dist-info/WHEEL,sha256=F01lGfVCzcXUzzQHzUkBmXAcu_TXd5zqMLrvrspncJo,85
@@ -81,13 +81,13 @@ experimental/examples/wheel/lib/module_with_data.py,sha256=K_IGAq_CHcZX0HUyINpD1
 experimental/examples/wheel/lib/simple_module.py,sha256=72-91Dm6NB_jw-7wYQt7shzdwvk5RB0LujIah8g7kr8,636
 experimental/examples/wheel/main.py,sha256=E0xCyiPg6fCo4IrFmqo_tqpNGtk1iCewobqD0_KlFd0,935
 """)
-            self.assertEquals(wheel_contents, b"""\
+            self.assertEqual(wheel_contents, b"""\
 Wheel-Version: 1.0
 Generator: wheelmaker 1.0
 Root-Is-Purelib: true
 Tag: py3-none-any
 """)
-            self.assertEquals(metadata_contents, b"""\
+            self.assertEqual(metadata_contents, b"""\
 Metadata-Version: 2.1
 Name: example_customized
 Version: 0.0.1
@@ -109,7 +109,7 @@ This is a sample description of a wheel.
                                 'example_custom_package_root-0.0.1-py3-none-any.whl')
 
         with zipfile.ZipFile(filename) as zf:
-            self.assertEquals(
+            self.assertEqual(
                 zf.namelist(),
                 ['examples/wheel/lib/data.txt',
                  'examples/wheel/lib/module_with_data.py',
@@ -126,7 +126,7 @@ This is a sample description of a wheel.
                                 'example_custom_package_root_multi_prefix-0.0.1-py3-none-any.whl')
 
         with zipfile.ZipFile(filename) as zf:
-            self.assertEquals(
+            self.assertEqual(
                 zf.namelist(),
                 ['data.txt',
                  'module_with_data.py',
@@ -143,7 +143,7 @@ This is a sample description of a wheel.
                                 'example_custom_package_root_multi_prefix_reverse_order-0.0.1-py3-none-any.whl')
 
         with zipfile.ZipFile(filename) as zf:
-            self.assertEquals(
+            self.assertEqual(
                 zf.namelist(),
                 ['lib/data.txt',
                  'lib/module_with_data.py',
