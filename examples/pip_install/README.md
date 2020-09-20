e267077908eacf61815155e2f936276e05ac2f2c
Alex Eagle <alex.eagle@robinhood.com>
2020-09-20 07:40:22 -0700

    move rules_python_external out of experimental
    - mv experimental/rules_python_external -> python/pip_install
    - mv examples/rules_python_external -> examples/pip_install
    
    This promotes rules_python_external as a public API in rules_python.
    In a follow-up PR we'll update the documentation to indicate this and instruct users how to switch.

---
+++ examples/pip_install/README.md
@@ -0,0 +1,4 @@
+# pip_install example
+
+This example shows how to use pip to fetch external dependencies from a requirements.txt file,
+then use them in BUILD files as dependencies of Bazel targets.
--- python/pip_install/extract_wheels/lib/requirements_test.py
+++ python/pip_install/extract_wheels/lib/requirements_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from experimental.rules_python_external.extract_wheels.lib import requirements
+from python.pip_install.extract_wheels.lib import requirements
 
 
 class TestRequirementExtrasParsing(unittest.TestCase):
