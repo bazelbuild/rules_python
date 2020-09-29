aacc7c2344dd60f6b7a49a1e226a77ec79da74be
Henry Fuller <hfuller@twitter.com>
2020-09-29 15:15:34 -0700

    Expose resolved .whl files in filegroup target of pypi__ packages.
    This allows distribution tools that have their own mechanisms of
    packaging whls to take advantage of the pip resolve and dependency
    graph creation that rules_python_external offers.

--- experimental/rules_python_external/extract_wheels/lib/bazel.py
+++ experimental/rules_python_external/extract_wheels/lib/bazel.py
@@ -3,12 +3,15 @@ import os
 import textwrap
 import json
 from typing import Iterable, List, Dict, Set
+import shutil
 
 from experimental.rules_python_external.extract_wheels.lib import namespace_pkgs, wheel, purelib
 
 
+WHEEL_FILE_LABEL = "whl"
+
 def generate_build_file_contents(
-    name: str, dependencies: List[str], pip_data_exclude: List[str]
+    name: str, dependencies: List[str], whl_file_deps: List[str], pip_data_exclude: List[str],
 ) -> str:
     """Generate a BUILD file for an unzipped Wheel
 
@@ -23,13 +26,19 @@ def generate_build_file_contents(
     there may be no Python sources whatsoever (e.g. packages written in Cython: like `pymssql`).
     """
 
-    data_exclude = ["**/*.py", "**/* *", "BUILD", "WORKSPACE"] + pip_data_exclude
+    data_exclude = ["*.whl", "**/*.py", "**/* *", "BUILD", "WORKSPACE"] + pip_data_exclude
 
     return textwrap.dedent(
         """\
         package(default_visibility = ["//visibility:public"])
 
         load("@rules_python//python:defs.bzl", "py_library")
+
+        filegroup(
+            name="{whl_file_label}",
+            srcs=glob(["*.whl"]),
+            data=[{whl_file_deps}]
+        )
 
         py_library(
             name = "{name}",
@@ -44,6 +53,8 @@ def generate_build_file_contents(
             name=name,
             dependencies=",".join(dependencies),
             data_exclude=json.dumps(data_exclude),
+            whl_file_label=WHEEL_FILE_LABEL,
+            whl_file_deps=",".join(whl_file_deps),
         )
     )
 
@@ -70,6 +81,9 @@ def generate_requirements_file_contents(repo_name: str, targets: Iterable[str]) 
         def requirement(name):
            name_key = name.replace("-", "_").replace(".", "_").lower()
            return "{repo}//pypi__" + name_key
+
+        def whl_requirement(name):
+            return requirement(name) + ":whl"
         """.format(
             repo=repo_name, requirement_labels=",".join(sorted(targets))
         )
@@ -125,7 +139,7 @@ def extract_wheel(
     pip_data_exclude: List[str],
     enable_implicit_namespace_pkgs: bool,
 ) -> str:
-    """Extracts wheel into given directory and creates a py_library target.
+    """Extracts wheel into given directory and creates py_library and filegroup targets.
 
     Args:
         wheel_file: the filepath of the .whl
@@ -141,6 +155,8 @@ def extract_wheel(
     directory = sanitise_name(whl.name)
 
     os.mkdir(directory)
+    # copy the original wheel
+    shutil.copy(whl.path, directory)
     whl.unzip(directory)
 
     # Note: Order of operations matters here
@@ -150,14 +166,18 @@ def extract_wheel(
         setup_namespace_pkg_compatibility(directory)
 
     extras_requested = extras[whl.name] if whl.name in extras else set()
+    whl_deps = sorted(whl.dependencies(extras_requested))
 
     sanitised_dependencies = [
-        '"//%s"' % sanitise_name(d) for d in sorted(whl.dependencies(extras_requested))
+        '"//%s"' % sanitise_name(d) for d in whl_deps
+    ]
+    sanitised_wheel_file_dependencies = [
+        '"//%s:%s"' % (sanitise_name(d), WHEEL_FILE_LABEL) for d in whl_deps
     ]
 
     with open(os.path.join(directory, "BUILD"), "w") as build_file:
         contents = generate_build_file_contents(
-            sanitise_name(whl.name), sanitised_dependencies, pip_data_exclude,
+            sanitise_name(whl.name), sanitised_dependencies, sanitised_wheel_file_dependencies, pip_data_exclude
         )
         build_file.write(contents)
 
