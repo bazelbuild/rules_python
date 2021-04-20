b82bde559b143e656517b32d2de288102986e566
Henry Fuller <hrofuller@gmail.com>
2021-04-19 19:46:10 -0700

    Add usage of whl_requirement to pip_parse example

--- examples/pip_parse/main.py
+++ examples/pip_parse/main.py
@@ -1,5 +1,9 @@
 import requests
+import glob
 
 
 def version():
     return requests.__version__
+
+def requests_wheels():
+    return glob.glob("external/**/*.whl")
--- examples/pip_parse/test.py
+++ examples/pip_parse/test.py
@@ -5,6 +5,7 @@ import main
 class ExampleTest(unittest.TestCase):
     def test_main(self):
         self.assertEqual("2.24.0", main.version())
+        self.assertTrue(len(main.requests_wheels()))
 
 
 if __name__ == '__main__':
