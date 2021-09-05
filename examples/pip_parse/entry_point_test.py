5ca44087f102b04ca1877f6c6431f435f1528130
UebelAndre <github@uebelandre.com>
2021-09-05 14:24:40 -0700

    Use yamllint for both pip_install and pip_parse

--- examples/pip_parse/entry_point_test.py
+++ examples/pip_parse/entry_point_test.py
@@ -9,11 +9,11 @@ class PipParseEntryPointTest(unittest.TestCase):
     def test_output(self):
         self.maxDiff = None
 
-        entry_point = Path("external/pip_parsed_deps_pypi__wheel/rules_python_wheel_entry_point_wheel")
+        entry_point = Path("external/pip_parsed_deps_pypi__yamllint/rules_python_wheel_entry_point_yamllint")
         self.assertTrue(entry_point.exists())
 
-        proc = subprocess.run([entry_point, "version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        self.assertEqual(proc.stdout.decode("utf-8").strip(), "wheel 0.37.0")
+        proc = subprocess.run([entry_point, "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.assertEqual(proc.stdout.decode("utf-8").strip(), "yamllint 1.26.3")
 
 
 if __name__ == "__main__":
