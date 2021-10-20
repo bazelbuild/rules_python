ed55f1ef64e37252bf276616292686fc1236a5cd
Gibson Fahnestock <gib@apple.com>
2021-10-20 20:52:03 +0100

    pip: entry_point: Add support for exit codes

---
+++ python/pip_install/extract_wheels/lib/bazel_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from python.pip_install.extract_wheels.lib.bazel import generate_entry_point_contents
+
+
+class BazelTestCase(unittest.TestCase):
+    def test_generate_entry_point_contents(self):
+        got = generate_entry_point_contents("sphinx.cmd.build:main")
+        want = """#!/usr/bin/env python3
+import sys
+from sphinx.cmd.build import main
+if __name__ == "__main__":
+    rc = main()
+    if rc.is_integer():
+        sys.exit(rc)
+"""
+        self.assertEqual(got, want)
+
+    def test_generate_entry_point_contents_with_shebang(self):
+        got = generate_entry_point_contents(
+            "sphinx.cmd.build:main", shebang="#!/usr/bin/python")
+        want = """#!/usr/bin/python
+import sys
+from sphinx.cmd.build import main
+rc = main()
+if rc.is_integer():
+    sys.exit(rc)
+"""
+        self.assertEqual(got, want)
