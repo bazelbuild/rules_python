d57aec1858c84eb7f778a5e186c3c30e7d700e34
UebelAndre <github@uebelandre.com>
2021-11-15 20:08:08 -0800

    Ignore `__pycache__` contents and `*.pyc` files in external dependencies

--- python/pip_install/extract_wheels/lib/bazel.py
+++ python/pip_install/extract_wheels/lib/bazel.py
@@ -104,7 +104,9 @@ def generate_build_file_contents(
 
     data_exclude = [
         "*.whl",
+        "**/__pycache__/**",
         "**/*.py",
+        "**/*.pyc",
         f"{WHEEL_ENTRY_POINT_PREFIX}*.py",
         "**/* *",
         "BUILD.bazel",
@@ -135,7 +137,7 @@ def generate_build_file_contents(
 
         py_library(
             name = "{name}",
-            srcs = glob(["**/*.py"], exclude=["{entry_point_prefix}*.py"], allow_empty = True),
+            srcs = glob(["**/*.py"], exclude=["{entry_point_prefix}*.py", "**/__pycache__/**"], allow_empty = True),
             data = glob(["**/*"], exclude={data_exclude}),
             # This makes this directory a top-level in the python import
             # search path for anything that depends on this.
