3891e54bd88865925873495559ed9f6948802ad5
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-10-05 12:08:38 -0700

    fix: black linter
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- examples/multi_python_versions/cross_version_test.py
+++ examples/multi_python_versions/cross_version_test.py
@@ -5,13 +5,16 @@ import sys
 process = subprocess.run(
     [os.getenv("SUBPROCESS_VERSION_PY_BINARY")],
     stdout=subprocess.PIPE,
-    universal_newlines=True)
+    universal_newlines=True,
+)
 
 subprocess_current = process.stdout.strip()
 subprocess_expected = os.getenv("SUBPROCESS_VERSION_CHECK")
 
 if subprocess_current != subprocess_expected:
-    print(f"expected subprocess version '{subprocess_expected}' is different than returned '{subprocess_current}'")
+    print(
+        f"expected subprocess version '{subprocess_expected}' is different than returned '{subprocess_current}'"
+    )
     sys.exit(1)
 
 expected = os.getenv("VERSION_CHECK")
