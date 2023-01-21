1722988cc407b08a4e7770295452076706823f9d
Alex Eagle <alex@aspect.dev>
2023-01-21 11:16:41 -0800

    chore: publish a runfiles library as a wheel (#995)
    * chore: publish a runfiles library as a wheel
    
    Wire it up to GH actions so it is published for each release.
    
    Tested locally with:
    bazel build python/runfiles:wheel --embed_label=1.0.2 --stamp
    PYTHONPATH=bazel-bin/python/runfiles/bazel_runfiles-_BUILD_EMBED_LABEL_-py3-none-any.whl python
    >>> import runfiles
    >>> runfiles.Create()
    
    Note, I would have liked to call the package bazel-runfiles, but this isn't possible without either refactoring the paths in this repo, or doing some fancy starlark to copy files around to create a folder that we turn into the wheel.
    There is no project https://pypi.org/project/runfiles though there is a https://pypi.org/project/runfile
    
    We could try harder to get the name we prefer.
    
    * Apply suggestions from code review
    
    Co-authored-by: Richard Levasseur <richardlev@gmail.com>
    
    * more code review cleanup
    
    Co-authored-by: Richard Levasseur <richardlev@gmail.com>

---
+++ python/runfiles/__init__.py
@@ -0,0 +1,1 @@
+from .runfiles import *
--- python/runfiles/runfiles.py
+++ python/runfiles/runfiles.py
@@ -14,49 +14,7 @@
 
 """Runfiles lookup library for Bazel-built Python binaries and tests.
 
-USAGE:
-
-1.  Depend on this runfiles library from your build rule:
-
-      py_binary(
-          name = "my_binary",
-          ...
-          deps = ["@rules_python//python/runfiles"],
-      )
-
-2.  Import the runfiles library.
-
-      from python.runfiles import runfiles
-
-3.  Create a Runfiles object and use rlocation to look up runfile paths:
-
-      r = runfiles.Create()
-      ...
-      with open(r.Rlocation("my_workspace/path/to/my/data.txt"), "r") as f:
-        contents = f.readlines()
-        ...
-
-    The code above creates a manifest- or directory-based implementations based
-    on the environment variables in os.environ. See `Create()` for more info.
-
-    If you want to explicitly create a manifest- or directory-based
-    implementations, you can do so as follows:
-
-      r1 = runfiles.CreateManifestBased("path/to/foo.runfiles_manifest")
-
-      r2 = runfiles.CreateDirectoryBased("path/to/foo.runfiles/")
-
-    If you want to start subprocesses that also need runfiles, you need to set
-    the right environment variables for them:
-
-      import subprocess
-      from bazel_tools.tools.python.runfiles import runfiles
-
-      r = runfiles.Create()
-      env = {}
-      ...
-      env.update(r.EnvVars())
-      p = subprocess.Popen([r.Rlocation("path/to/binary")], env, ...)
+See README.md for usage instructions.
 """
 import inspect
 import os
