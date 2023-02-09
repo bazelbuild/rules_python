bd26d718cd362b8aa6b9c3e8719febf31250e46d
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2023-02-09 09:56:15 +0900

    fix: add protobuf via requirements.txt

--- examples/py_proto_library/test.py
+++ examples/py_proto_library/test.py
@@ -6,7 +6,7 @@ import pricetag_pb2
 
 class TestCase(unittest.TestCase):
     def test_pricetag(self):
-        got = pricetag_pb2.Pricetag(
+        got = pricetag_pb2.PriceTag(
             name="dollar",
             cost=5.00,
         )
