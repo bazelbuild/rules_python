643a14bf80179b2eb96de022344b850047788b51
Zhuo Peng <zhuo.peng@snowflake.com>
2023-06-16 14:45:39 -0700

    feat(py_wheel): Add support for specifying Description-Content-Type and Summary in METADATA (#1274)
    `py_wheel` allows to supply a description file, but it does not allow
    specifying the content type of that file.
    By default, the type is "text/x-rst", but many packages are using a
    markdown description.
    
    
    https://packaging.python.org/en/latest/specifications/core-metadata/#description-content-type
    
    This change added the support.
    
    ---------
    
    Co-authored-by: Richard Levasseur <richardlev@gmail.com>

--- examples/wheel/wheel_test.py
+++ examples/wheel/wheel_test.py
@@ -99,7 +99,7 @@ class WheelTest(unittest.TestCase):
                 record_contents,
                 # The entries are guaranteed to be sorted.
                 b"""\
-example_customized-0.0.1.dist-info/METADATA,sha256=YUnzQ9gTMXspIBURe90Ct3aL_CCn8fwC3SiZe6MMTs8,372
+example_customized-0.0.1.dist-info/METADATA,sha256=vRiyyV45PC5fzK_40nSTtIn3yYzDdsbBAbUvkZiRyc8,461
 example_customized-0.0.1.dist-info/NOTICE,sha256=Xpdw-FXET1IRgZ_wTkx1YQfo1-alET0FVf6V1LXO4js,76
 example_customized-0.0.1.dist-info/README,sha256=WmOFwZ3Jga1bHG3JiGRsUheb4UbLffUxyTdHczS27-o,40
 example_customized-0.0.1.dist-info/RECORD,,
@@ -129,6 +129,8 @@ Author: Example Author with non-ascii characters: \xc5\xbc\xc3\xb3\xc5\x82w
 Author-email: example@example.com
 Home-page: www.example.com
 License: Apache 2.0
+Description-Content-Type: text/markdown
+Summary: A one-line summary of this test package
 Classifier: License :: OSI Approved :: Apache Software License
 Classifier: Intended Audience :: Developers
 Requires-Dist: pytest
