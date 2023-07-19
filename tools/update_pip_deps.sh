5a579cb2a55ca51e20ffb081398486dbcd10abd1
Ignas Anikevicius <anikevicius@gmail.com>
2023-07-19 09:22:17 +0900

    comment: add a check to ensure that dependencies exist

--- tools/update_pip_deps.sh
+++ tools/update_pip_deps.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
+# Copyright 2023 The Bazel Authors. All rights reserved.
 #
-# A script to manage internal dependencies
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A script to manage internal pip dependencies
 
 readonly ROOT="$(dirname "$0")"/..
 readonly START="# START: maintained by //tools/update_pip_deps.sh"
 readonly END="# END: maintained by //tools/update_pip_deps.sh"
+
+_assert_exists() {
+    if ! type $1 >/dev/null; then
+        echo "Please install '$1' to use this script"
+        exit 1
+    fi
+}
+
+_assert_exists jq
+_assert_exists sed
+_assert_exists awk
+_assert_exists python
 
 pip_args=(
     install
