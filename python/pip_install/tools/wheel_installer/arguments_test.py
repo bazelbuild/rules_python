67f43b5ca6cbded77ed6a0d18ef3749818386812
Ignas Anikevicius <anikevicius@gmail.com>
2023-07-21 09:54:27 +0900

    refactor(whl_library): move bazel file generation to Starlark
    Before this PR, the `wheel_installer` was doing three things:
    1. Downloading the right wheel.
    2. Extracting it into the output directory.
    3. Generating BUILD.bazel files based on the extracted contents.
    
    This PR is moving the third part into the `whl_library` repository rule
    and it has the following benefits:
    * We can reduce code duplication and label sanitization functions in
      rules_python.
    * There are many things that the `wheel_installer` does not care anymore
      and we don't need to change less code when extending `whl_library` as
      we can now do many things in starlark directly.
    * It becomes easier to change the API of how we expose the generated
      BUILD.bazel patching because we only need to change the Starlark
      functions.
    
    Work towards #1330.

--- python/pip_install/tools/wheel_installer/arguments_test.py
+++ python/pip_install/tools/wheel_installer/arguments_test.py
@@ -16,35 +16,30 @@ import argparse
 import json
 import unittest
 
-from python.pip_install.tools.lib import arguments
+from python.pip_install.tools.wheel_installer import arguments
 
 
 class ArgumentsTestCase(unittest.TestCase):
     def test_arguments(self) -> None:
-        parser = argparse.ArgumentParser()
-        parser = arguments.parse_common_args(parser)
+        parser = arguments.parser()
         repo_name = "foo"
         repo_prefix = "pypi_"
         index_url = "--index_url=pypi.org/simple"
         extra_pip_args = [index_url]
+        requirement = "foo==1.0.0 --hash=sha256:deadbeef"
         args_dict = vars(
             parser.parse_args(
                 args=[
-                    "--repo",
-                    repo_name,
+                    f'--requirement="{requirement}"',
                     f"--extra_pip_args={json.dumps({'arg': extra_pip_args})}",
-                    "--repo-prefix",
-                    repo_prefix,
                 ]
             )
         )
         args_dict = arguments.deserialize_structured_args(args_dict)
-        self.assertIn("repo", args_dict)
+        self.assertIn("requirement", args_dict)
         self.assertIn("extra_pip_args", args_dict)
         self.assertEqual(args_dict["pip_data_exclude"], [])
         self.assertEqual(args_dict["enable_implicit_namespace_pkgs"], False)
-        self.assertEqual(args_dict["repo"], repo_name)
-        self.assertEqual(args_dict["repo_prefix"], repo_prefix)
         self.assertEqual(args_dict["extra_pip_args"], extra_pip_args)
 
     def test_deserialize_structured_args(self) -> None:
