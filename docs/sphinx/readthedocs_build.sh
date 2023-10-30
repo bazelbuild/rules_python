65a51ab8ac95a4707b9fcc32f20c27e22c081b2d
Richard Levasseur <rlevasseur@google.com>
2023-10-30 14:28:18 -0700

    fixup! docs: show PR warning banner and fix links doc source pages

--- docs/sphinx/readthedocs_build.sh
+++ docs/sphinx/readthedocs_build.sh
@@ -8,6 +8,9 @@ while IFS='=' read -r -d '' name value; do
   fi
 done < <(env -0)
 
+# In order to get the build number, we extract it from the host name
+extra_env+=("--//sphinxdocs:extra_env=HOSTNAME=$HOSTNAME")
+
 set -x
 bazel run \
   "--//sphinxdocs:extra_defines=version=$READTHEDOCS_VERSION" \
