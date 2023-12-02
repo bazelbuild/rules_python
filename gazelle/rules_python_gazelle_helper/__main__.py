72372c34541681a55de7ac24b913000d5a7dcc92
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2023-12-03 00:15:34 +0900

    fix(gazelle): use Python 3.11 in tests and use absolute paths in helper
    Before this change there was a bug in how the parsing helpers were being
    used in case we were using Python 3.11 toolchain, which is using a more
    strict version of the entrypoint template. This change moves the python
    code to a different location to ensure that the top level package is
    something more unique than just "python" and updates the non-bzlmod
    tests to run under 3.11.
    
    We also change ".bazelrc" to use explicit "__init__.py" definition to
    avoid non-reproducible errors in the future.
    
    Fixes #1589

--- gazelle/rules_python_gazelle_helper/__main__.py
+++ gazelle/rules_python_gazelle_helper/__main__.py
@@ -16,9 +16,15 @@
 # STDIN receives parse requests, one per line. It outputs the parsed modules and
 # comments from all the files from each request.
 
-import parse
-import std_modules
 import sys
+
+# NOTE @aignas 2023-12-02: Use absolute imports with respect to WORKSPACE root.
+# With Python versions other than 3.11 doing import parse import std_modules
+# works fine, but with 3.11 we need to use absolute import paths, which could be
+# due to differences in the bootstrap template in 3.11, which is more strict.
+#
+# We are also using a unique name to avoid any name clashes
+from rules_python_gazelle_helper import parse, std_modules
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
