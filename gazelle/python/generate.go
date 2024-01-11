11640e32e355bb8418483a2eaa6ae5eacf129fec
Arya McCarthy <amccarthy@scaledcognition.com>
2024-01-10 18:10:24 -0800

    Fix unused var issue

--- gazelle/python/generate.go
+++ gazelle/python/generate.go
@@ -212,7 +212,6 @@ func (py *Python) GenerateRules(args language.GenerateArgs) language.GenerateRes
 	}
 
 	parser := newPython3Parser(args.Config.RepoRoot, args.Rel, cfg.IgnoresDependency)
-	visibility := fmt.Sprintf("//%s:__subpackages__", pythonProjectRoot)
 
 	var result language.GenerateResult
 	result.Gen = make([]*rule.Rule, 0)
