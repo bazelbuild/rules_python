fd477fc380f3e147c09570a9a730b8ef3d1bb469
Philipp Schrader <philipp.schrader@gmail.com>
2024-02-11 21:31:08 -0800

    Try it out with multiple Python versions.

--- examples/pypi_install/update_helper/README.md
+++ examples/pypi_install/update_helper/README.md
@@ -8,12 +8,5 @@ sudo apt install qemu-user-static
 Then update for x86:
 
 ```
-export DOCKER_BUILDKIT=1
-bazel run //:requirements.update --run_under="//update_helper linux/amd64" --platforms=//platforms:linux_x86
-```
-
-And update for arm64:
-```
-export DOCKER_BUILDKIT=1
-bazel run //:requirements.update --run_under="//update_helper linux/arm64/v8" --platforms=//platforms:linux_aarch64
+./update_helper/update_all.sh
 ```
---
+++ examples/pypi_install/update_helper/update_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+export DOCKER_BUILDKIT=1
+
+set -x
+for py in py310 py311; do
+  bazel run \
+    --config="${py}" \
+    --run_under="//update_helper linux/amd64" \
+    --platforms=//platforms:linux_x86 \
+    //:requirements.update
+  bazel run \
+    --config="${py}" \
+    --run_under="//update_helper linux/arm64/v8" \
+    --platforms=//platforms:linux_aarch64 \
+    //:requirements.update
+done
