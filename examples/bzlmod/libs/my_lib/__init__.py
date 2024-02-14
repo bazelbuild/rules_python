b00dba49665522ba75f0ef51238593f4828e7d5a
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2024-02-14 13:28:00 +0900

    test(bzlmod): make the check for hub repo naming less strict
    When adding a whl-only hub that gets selected for a particular
    platform only, this check needed to be adjusted (as seen in #1744).

--- examples/bzlmod/libs/my_lib/__init__.py
+++ examples/bzlmod/libs/my_lib/__init__.py
@@ -17,6 +17,6 @@ import websockets
 
 def websockets_is_for_python_version(sanitized_version_check):
     # We are checking that the name of the repository folders
-    # match the expexted generated names. If we update the folder
-    # structure or naming we will need to modify this test
-    return f"pip_{sanitized_version_check}_websockets" in websockets.__file__
+    # match the expected generated names. If we update the folder
+    # structure or naming we will need to modify this test.
+    return f"_{sanitized_version_check}_websockets" in websockets.__file__
