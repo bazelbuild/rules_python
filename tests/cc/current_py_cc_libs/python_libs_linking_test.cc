5f4179e9e91661415bf7f5f502d92e5f3f05d872
Nicholas Junge <nicholas.junge@web.de>
2024-04-08 20:39:01 +0200

    Use envvar instead of argc for early return

--- tests/cc/current_py_cc_libs/python_libs_linking_test.cc
+++ tests/cc/current_py_cc_libs/python_libs_linking_test.cc
@@ -1,8 +1,10 @@
+#include <cstdlib>
 #include <Python.h>
 
 int main(int argc, char** argv) {
   // Early return to prevent the broken code below from running.
-  if (argc >= 1) {
+  char* val = std::getenv("HELLO");
+  if (val || argc >= 1) {
     return 0;
   }
 
