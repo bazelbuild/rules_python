aad3cc909125bd081466ebcf5b52439886b3cee1
Laurenz <git@laure.nz>
2024-05-18 11:39:24 +0200

    Use custom wheel instead of numpy for integration test

--- tests/integration/whl_filegroup/README.md
+++
@@ -1,3 +0,0 @@
-# whl_filegroup
-
-This test checks that whl_filegroup can be used to depend on arbitrary wheel files.
--- tests/integration/whl_filegroup/test_using_numpy_headers.c
+++
@@ -1,6 +0,0 @@
-#include <numpy/npy_common.h>
-
-int main(int argc, char**argv) {
-    npy_int retval = 0;
-    return retval;
-}
