be8d2c798505294988f7ee76661d6855bf2deacb
Richard Levasseur <rlevasseur@google.com>
2024-07-18 14:02:49 -0700

    fix ci

--- tests/base_rules/sys_path_order_test.py
+++ tests/base_rules/sys_path_order_test.py
@@ -21,9 +21,13 @@ import unittest
 # pythonX.Y, pythonXY.zip, and pythonX.Y/lib-dynload
 # See https://docs.python.org/3/library/sys_path_init.html#sys-path-init
 # for details.
-_STDLIB_REGEX = r".*python\d[.]\d+$|.*python\d+[.]zip$|.*lib-dynload$"
+_STDLIB_REGEX = r".*python\d[.]\d+$|.*python\d+[.]zip$|.*lib-dynload$|"
 
-_STDLIB_SITE_PACKAGES_REGEX = r".*python\d[.]\d+/(site|dist)-packages$"
+
+# Windows: `{sys.prefix}/Lib/site-packages"
+# Others: `
+_STDLIB_SITE_PACKAGES_REGEX = r".*(python\d[.]\d+|Lib)/(site|dist)-packages$"
+
 
 class SysPathOrderTest(unittest.TestCase):
     def test_sys_path_order(self):
@@ -31,23 +35,34 @@ class SysPathOrderTest(unittest.TestCase):
         first_user = None
         first_runtime_site = None
         for i, value in enumerate(sys.path):
-            value = value.replace(os.path.sep, "/")
-            if re.match(_STDLIB_REGEX, value):
-                last_stdlib = i
-            elif re.match(_STDLIB_SITE_PACKAGES_REGEX, value):
-                if first_runtime_site is None:
-                    first_runtime_site = i
-            elif ".runfiles" in value:
-                if first_user is None:
-                    first_user = i
-            else:
-                raise AssertionError(f"Unexpected sys.path format: {value}")
-
-        assert last_stdlib is not None, sys.path
-        assert first_user is not None, sys.path
-        assert first_runtime_site is not None, sys.path
+            # Normalize windows paths to unix paths
+            ##value = value.replace(os.path.sep, "/")
+            if value.startswith(sys.prefix):
+                if os.path.basename(value).endswith("-packages"):
+                    if first_runtime_site is None:
+                        first_runtime_site = i
+                else:
+                    last_stdlib = i
+            elif first_user is None:
+                first_user = i
+            ##if re.match(_STDLIB_REGEX, value):
+            ##    last_stdlib = i
+            ##elif re.match(_STDLIB_SITE_PACKAGES_REGEX, value):
+            ##    if first_runtime_site is None:
+            ##        first_runtime_site = i
+            ##elif ".runfiles" in value:
+            ##    if first_user is None:
+            ##        first_user = i
+            ##else:
+            ##    raise AssertionError(f"Unexpected sys.path format: {value}")
 
         sys_path_str = "\n".join(f"{i}: {v}" for i, v in enumerate(sys.path))
+        if None in (last_stdlib, first_user, first_runtime_site):
+            self.fail(
+                "Failed to find position for one of:\n"
+                + f"{last_stdlib=} {first_user=} {first_runtime_site=}\n"
+                + f"for sys.path:\n{sys_path_str}"
+            )
 
         if os.environ["BOOTSTRAP"] == "script":
             self.assertTrue(
