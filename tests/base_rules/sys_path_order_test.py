6999e7f318bb9253f75fd5545b1985c802640ec2
Richard Levasseur <rlevasseur@google.com>
2024-07-18 14:21:22 -0700

    fix ci

--- tests/base_rules/sys_path_order_test.py
+++ tests/base_rules/sys_path_order_test.py
@@ -34,29 +34,43 @@ class SysPathOrderTest(unittest.TestCase):
         last_stdlib = None
         first_user = None
         first_runtime_site = None
+
+        # Classify paths into the three different types we care about: stdlib,
+        # user dependency, or the runtime's site-package's directory.
+        # Because they often share common prefixes and vary subtly between
+        # platforms, we do this is two passes: first category, then compute
+        # the indexes. This is just so debugging is easier, especially
+        # for platforms a dev doesn't have.
+        categorized_paths = []
         for i, value in enumerate(sys.path):
             # Normalize windows paths to unix paths
             ##value = value.replace(os.path.sep, "/")
-            if value.startswith(sys.prefix):
+            if value == sys.prefix:
+                category = "user"
+            elif value.startswith(sys.prefix):
                 if os.path.basename(value).endswith("-packages"):
-                    if first_runtime_site is None:
-                        first_runtime_site = i
+                    category = "runtime-site"
                 else:
-                    last_stdlib = i
-            elif first_user is None:
-                first_user = i
-            ##if re.match(_STDLIB_REGEX, value):
-            ##    last_stdlib = i
-            ##elif re.match(_STDLIB_SITE_PACKAGES_REGEX, value):
-            ##    if first_runtime_site is None:
-            ##        first_runtime_site = i
-            ##elif ".runfiles" in value:
-            ##    if first_user is None:
-            ##        first_user = i
-            ##else:
-            ##    raise AssertionError(f"Unexpected sys.path format: {value}")
+                    category = "stdlib"
+            else:
+                category = "user"
 
-        sys_path_str = "\n".join(f"{i}: {v}" for i, v in enumerate(sys.path))
+            categorized_paths.append((category, value))
+
+        for i, (category, _) in categorized_paths:
+            if category == "stdlib":
+                last_stdlib = i
+            elif category == "runtime-site":
+                if first_runtime_site is None:
+                    first_runtime_site = i
+            elif category == "user":
+                if first_user is None:
+                    first_user = i
+
+        sys_path_str = "\n".join(
+            f"{i}: ({category}) {value}"
+            for i, (category, value) in enumerate(categorized_paths)
+        )
         if None in (last_stdlib, first_user, first_runtime_site):
             self.fail(
                 "Failed to find position for one of:\n"
