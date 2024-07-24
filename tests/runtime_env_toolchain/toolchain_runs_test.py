0f7e1feee11b76ef249cdf8b62d6b090f9dd9357
Richard Levasseur <rlevasseur@google.com>
2024-07-23 18:54:33 -0700

    fixing runfiles lookup for workspace

--- tests/runtime_env_toolchain/toolchain_runs_test.py
+++ tests/runtime_env_toolchain/toolchain_runs_test.py
@@ -1,7 +1,7 @@
 import json
 import pathlib
-import unittest
 import platform
+import unittest
 
 from python.runfiles import runfiles
 
@@ -9,14 +9,19 @@ from python.runfiles import runfiles
 class RunTest(unittest.TestCase):
     def test_ran(self):
         rf = runfiles.Create()
-        settings_path = rf.Rlocation("_main/tests/support/current_build_settings.json")
+        settings_path = rf.Rlocation(
+            "rules_python/tests/support/current_build_settings.json"
+        )
         settings = json.loads(pathlib.Path(settings_path).read_text())
-        import sys
-        print("===Settings:", settings, file=sys.stderr)
         if platform.system == "Windows":
-            self.assertEqual("/_magic_pyruntime_sentinel_do_not_use", settings["interpreter_path"])
+            self.assertEqual(
+                "/_magic_pyruntime_sentinel_do_not_use", settings["interpreter_path"]
+            )
         else:
-            self.assertIn("runtime_env_toolchain_interpreter.sh", settings["interpreter"]["short_path"])
+            self.assertIn(
+                "runtime_env_toolchain_interpreter.sh",
+                settings["interpreter"]["short_path"],
+            )
 
 
 if __name__ == "__main__":
