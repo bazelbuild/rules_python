3804bbb84874302ff47e30ba67e175eac8ba5eae
Alex Torok <alext9@gmail.com>
2024-08-27 20:25:00 +0000

    Fix explicit package overrides that overlap with a third party dependency module

--- gazelle/python/testdata/dependency_resolution_order/third_party/baz.py
+++ gazelle/python/testdata/dependency_resolution_order/third_party/baz.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The Bazel Authors. All rights reserved.
+# Copyright 2024 The Bazel Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,21 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+import os
 
-import bar
-import baz
-import foo
-
-# Ensure that even though @gazelle_python_test//other_pip_dep provides "third_party",
-# we can still override "third_party.foo.bar"
-import third_party.foo.bar
-
-from third_party import baz
-
-import third_party
-
-_ = sys
-_ = bar
-_ = baz
-_ = foo
+_ = os
